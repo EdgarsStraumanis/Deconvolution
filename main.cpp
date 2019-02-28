1321354393ca973f2b644154200f4e3c0f422428
Edgars <edgars.straumanis@gmail.com>
2019-02-28 14:12:20 +0200

    Start of linked list
    Making linked list to store experimental data

--- main.cpp
+++ main.cpp
@@ -2,19 +2,62 @@
 
 using namespace std;
 
-struct dataTable{
-double time;
-int intensity;
-dataTable* next = 0;
-dataTable* previous = 0;
+struct dataPair{
+    double time;
+    int intensity;
+    dataPair* next = 0;
+    dataPair* previous = 0;
+};
+
+struct linkedList{
+    dataPair* first = 0;
+    dataPair* last = 0;
+    int dataPointCount = 0;
+
+    void addPair(double makeTime, int makeIntensity){
+        dataPair* creatingPair = new dataPair;
+        creatingPair->time = makeTime;
+        creatingPair->intensity = makeIntensity;
+        if (dataPointCount == 0){
+            first = creatingPair;
+        }
+        else
+        {
+            last->next = creatingPair;
+        }
+        dataPointCount++;
+        last = creatingPair;
+    }
+
+    void clearList(){
+        dataPair* deletePair = first;
+        while (deletePair != 0){
+            dataPair* saveNext = deletePair->next;
+            first = saveNext;
+            delete deletePair;
+            deletePair = saveNext;
+            dataPointCount--;
+        }
+        first = 0;
+        last = 0;
+    }
 };
 
 int main()
 {
-    dataTable* firstLaser = new dataTable;
-    dataTable pointerLaser = *firstLaser;
-    pointerLaser.time = 1;
-    pointerLaser.intensity = 2;
-    cout << pointerLaser.time << endl;
+    /*
+    dataPair* firstLaser = new dataPair;
+    firstLaser->time = 1;
+    firstLaser->intensity = 2;
+    cout << firstLaser->time << endl;
+    delete firstLaser;
+    if (firstLaser) cout << firstLaser->time << endl; else cout << "doesn't exist" << endl;
+    */
+    linkedList* testList = new linkedList;
+    testList->addPair(1,2);
+    testList->addPair(3,4);
+    cout << testList->dataPointCount << endl;
+    testList->clearList();
+    cout << testList->dataPointCount << endl;
     return 0;
 }
